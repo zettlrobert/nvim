a2ef91ae6e61c5e4ca948ba3a1213b8b9275de98
zettlrobert <zettl.robert@gmail.com>
2021-03-27 16:28:52 +0100

    using default lsp settings

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -23,3 +23,31 @@ vim.fn.sign_define("LspDiagnosticsSignInformation", {
 		numhl = "LspDiagnosticsSignInformation"
 })
 
+
+-- config defaults
+-- -----------------------------------------------------------------
+local nvim_lsp = require('lspconfig')
+local on_attach = function(client, bufnr)
+
+  -- Set autocommands conditional on server_capabilities
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec([[
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=#5e81ac
+      hi LspReferenceText cterm=bold ctermbg=red guibg=#5e81ac
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#5e81ac
+
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+  end
+end
+
+-- Use a loop to conveniently both setup defined servers 
+-- and map buffer local keybindings when the language server attaches
+local servers = { "pyright", "rust_analyzer", "tsserver" }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup { on_attach = on_attach }
+end
